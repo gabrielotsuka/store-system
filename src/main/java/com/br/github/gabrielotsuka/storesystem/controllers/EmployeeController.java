837b89761446c0ce3a0a7c8af8e42cf11f84a2cf
gabrielotsuka <gabrielotsuka@gmail.com>
2019-12-27 13:25:22 -0300

    insertion and listing employees working

--- src/main/java/com/br/github/gabrielotsuka/storesystem/controllers/EmployeeController.java
+++ src/main/java/com/br/github/gabrielotsuka/storesystem/controllers/EmployeeController.java
@@ -4,6 +4,8 @@ import com.br.github.gabrielotsuka.storesystem.models.Employee;
 import com.br.github.gabrielotsuka.storesystem.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,9 +28,11 @@ public class EmployeeController {
         return "redirect:/admin/employees/register";
     }
 
-    @RequestMapping(value = "admin/employees/list",method = RequestMethod.GET)
-    public String list() {
-        return "admin/employees/list";
+    @GetMapping("/admin/employees/list")
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("admin/employees/list");
+        mv.addObject("listEmployees", employeeRepository.findAll());
+        return mv;
     }
 
 }
