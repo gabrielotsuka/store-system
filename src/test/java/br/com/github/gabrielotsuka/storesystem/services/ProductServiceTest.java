b568031d33d1ddd57ac0bc8dd4aca3a74f49632c
gabrielotsuka <gabrielotsuka@gmail.com>
2020-02-03 17:15:01 -0300

    Setting up to unitary tests

--- src/test/java/br/com/github/gabrielotsuka/storesystem/services/ProductServiceTest.java
+++ src/test/java/br/com/github/gabrielotsuka/storesystem/services/ProductServiceTest.java
@@ -1,26 +1,45 @@
 package br.com.github.gabrielotsuka.storesystem.services;
 
-import org.junit.Before;
+import br.com.github.gabrielotsuka.storesystem.models.Product;
+import br.com.github.gabrielotsuka.storesystem.repositories.ProductRepository;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @RunWith(SpringRunner.class)
 public class ProductServiceTest {
 
-    @Before
-    public void setup(){
-        System.out.println("teste");
-    }
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private Product product;
+
+//    @Before
+//    public void setUp(){
+//
+//    }
 
     @Test
-    public void initialTest(){
+    public void saveTest_blankName() throws MethodArgumentNotValidException {
+        //cenário
+        product = new Product(" ", 1.5, 100);
+        Mockito.when(productRepository.save(product)).thenThrow(MethodArgumentNotValidException.class);
 
+        //ação
+        productService.saveProduct(product);
+
+        //verificação
     }
-
-    @Test
-    public void quaseInitialTest(){
-
-    }
-
 }
